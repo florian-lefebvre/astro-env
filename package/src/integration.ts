1d89a1db878e494c99695f7edd589b202932f3e0
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-01-23 18:18:15 +0100

    chore: work on using defineIntegration

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -1,10 +1,63 @@
 import type { AstroIntegration } from "astro";
-import { createResolver, watchIntegration } from "astro-integration-kit";
+import {
+	createResolver,
+	defineIntegration,
+	watchIntegration,
+} from "astro-integration-kit";
 import { loadEnv } from "vite";
 import { generateEnvTemplate } from "./generate-env-template.js";
 import { generateSchemaTypes } from "./generate-schema-types.js";
 import type { Options } from "./types.js";
 import { validateEnv } from "./validate-env.js";
+import { z } from "astro/zod";
+
+export const _integration = defineIntegration({
+	name: "astro-env",
+	options: z.object({
+		schema: z.object,
+		generateTypes: z.boolean().optional().default(true),
+		generateEnvTemplate: z.boolean().optional().default(false),
+	}).default({
+		schema: z.object({})
+	}),
+	setup(options) {
+		const { resolve } = createResolver(import.meta.url);
+
+		return {
+			"astro:config:setup": async ({ command, logger }) => {
+				await watchIntegration(resolve());
+
+				validateEnv({
+					schema: options.schema,
+					command,
+					logger,
+					env: loadEnv(
+						command === "dev" ? "development" : "production",
+						process.cwd(),
+						"",
+					),
+				});
+			},
+			"astro:config:done": async ({ config, logger }) => {
+				if (options.generateTypes) {
+					await generateSchemaTypes({
+						root: config.root,
+						srcDir: config.srcDir,
+						logger,
+						schema: options.schema,
+					});
+				}
+				if (options.generateEnvTemplate) {
+					await generateEnvTemplate({
+						root: config.root,
+						logger,
+						schema: options.schema,
+					});
+				}
+			},
+		};
+	},
+});
 
 export const integration = ({
 	schema: _schema,
