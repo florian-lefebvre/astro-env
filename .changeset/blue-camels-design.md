9fc5b1599873268203cd6c7ca2a803808974b6fc
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-06-07 12:29:14 +0200

    feat: deprecate

---
+++ .changeset/blue-camels-design.md
@@ -0,0 +1,5 @@
+---
+"astro-env": patch
+---
+
+Deprecates the package. Astro 4.10 now has `astro:env` in experimental: https://astro.build/blog/astro-4100/
--- package/src/integration.ts
+++ package/src/integration.ts
@@ -6,63 +6,68 @@ import { generateSchemaTypes } from "./generate-schema-types.js";
 import type { Options } from "./types.js";
 import { validateEnv } from "./validate-env.js";
 
+/** @deprecated Astro 4.10 now has `astro:env` in experimental: https://astro.build/blog/astro-4100/ */
 export const integration = ({
-	schema: _schema,
-	generateTypes: _generateTypes = true,
-	generateEnvTemplate: _generateEnvTemplate = false,
+  schema: _schema,
+  generateTypes: _generateTypes = true,
+  generateEnvTemplate: _generateEnvTemplate = false,
 }: Options): AstroIntegration => {
-	const options = {
-		schema: _schema,
-		generateTypes: _generateTypes,
-		generateEnvTemplate: _generateEnvTemplate,
-	};
+  const options = {
+    schema: _schema,
+    generateTypes: _generateTypes,
+    generateEnvTemplate: _generateEnvTemplate,
+  };
 
-	const { resolve } = createResolver(import.meta.url);
+  const { resolve } = createResolver(import.meta.url);
 
-	return {
-		name: "astro-env",
-		hooks: {
-			"astro:config:setup": ({
-				addWatchFile,
-				command,
-				logger,
-				updateConfig,
-			}) => {
-				watchIntegration({
-					addWatchFile,
-					command,
-					dir: resolve(),
-					updateConfig,
-				});
+  return {
+    name: "astro-env",
+    hooks: {
+      "astro:config:setup": ({
+        addWatchFile,
+        command,
+        logger,
+        updateConfig,
+      }) => {
+        logger.error(
+          "This integration is deprecated. Astro 4.10 now has \`astro:env\` in experimental: https://astro.build/blog/astro-4100/"
+        );
 
-				validateEnv({
-					schema: options.schema,
-					command,
-					logger,
-					env: loadEnv(
-						command === "dev" ? "development" : "production",
-						process.cwd(),
-						"",
-					),
-				});
-			},
-			"astro:config:done": async ({ config, logger }) => {
-				if (options.generateTypes) {
-					await generateSchemaTypes({
-						root: config.root,
-						srcDir: config.srcDir,
-						logger,
-						schema: options.schema,
-					});
-				}
-				if (options.generateEnvTemplate) {
-					await generateEnvTemplate({
-						root: config.root,
-						logger,
-						schema: options.schema,
-					});
-				}
-			},
-		},
-	};
+        watchIntegration({
+          addWatchFile,
+          command,
+          dir: resolve(),
+          updateConfig,
+        });
+
+        validateEnv({
+          schema: options.schema,
+          command,
+          logger,
+          env: loadEnv(
+            command === "dev" ? "development" : "production",
+            process.cwd(),
+            ""
+          ),
+        });
+      },
+      "astro:config:done": async ({ config, logger }) => {
+        if (options.generateTypes) {
+          await generateSchemaTypes({
+            root: config.root,
+            srcDir: config.srcDir,
+            logger,
+            schema: options.schema,
+          });
+        }
+        if (options.generateEnvTemplate) {
+          await generateEnvTemplate({
+            root: config.root,
+            logger,
+            schema: options.schema,
+          });
+        }
+      },
+    },
+  };
 };
